b1ff510c93a8c917f797d859b7b8af7648c0ac14
bayesdj <jack.liu@evolv.ai>
2022-08-18 11:24:33 -0700

    1st commit

---
+++ api_fred01.py
@@ -0,0 +1,17 @@
+import requests
+#%%
+api_key = '7817752224816662f4f155d53988793f'
+series_id = 'MEDCPIM158SFRBCLE'
+# fred_url = 'https://api.stlouisfed.org/fred/series/release?file_type=json'
+fred_url = 'https://api.stlouisfed.org/fred/series/observations?file_type=json&limit=1&offset=0&sort_order=desc&units=lin'
+api_url = fred_url + '&series_id=' + series_id + '&api_key=' + api_key
+
+
+
+response = requests.get(api_url)
+if response.status_code == requests.codes.ok:
+    print(response.text)
+else:
+    print("Error:", response.status_code, response.text)
+# %%
+a = 3 
