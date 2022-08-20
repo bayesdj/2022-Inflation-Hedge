a95700e557473f71ecebeee64ec4b55808727cdb
bayesdj <jack.liu@evolv.ai>
2022-08-20 13:20:01 -0700

    added ea contract and created ea folder

---
+++ contracts/contracts/jackLiu_post_uint256.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+// block-farms.io
+// Discord=https://discord.gg/PgxRVrDUm7
+
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+
+contract postUintTemplate is ChainlinkClient, ConfirmedOwner {
+  using Chainlink for Chainlink.Request;
+
+  uint256 public Uint;
+
+  bytes32 private externalJobId;
+  uint256 private oraclePayment;
+  address private oracle;
+  
+  event RequestUintFulfilled(bytes32 indexed requestId, uint256 indexed Uint);
+
+  constructor() ConfirmedOwner(msg.sender){
+    setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
+    oracle = 0x103Ca41fa1257d5014b04b6B212B251e9E7A740a;
+    externalJobId = "83470cd3869244cca1181adc6130b54d";
+    oraclePayment = (0.0 * LINK_DIVISIBILITY); // n * 10**18
+  }
+
+  function requestUint()
+    public
+    onlyOwner
+  {
+    Chainlink.Request memory req = buildChainlinkRequest(externalJobId, address(this), this.fulfillUint.selector);
+    req.add("path", "data,result");
+    req.addInt("times", 10000000000000000);
+    sendChainlinkRequestTo(oracle, req, oraclePayment);
+  }
+
+  function fulfillUint(bytes32 _requestId, uint256 _Uint)
+    public
+    recordChainlinkFulfillment(_requestId)
+  {
+    emit RequestUintFulfilled(_requestId, _Uint);
+    Uint = _Uint;
+  }
+
+}
